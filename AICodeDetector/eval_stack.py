6ac0d80fda106cd7188361f1b736cfdf7b07d170
kato0209 <kato.921002@gmail.com>
2024-05-22 15:18:44 +0900

    fix

--- AICodeDetector/eval_stack.py
+++ AICodeDetector/eval_stack.py
@@ -1,11 +1,6 @@
 from  datasets  import  load_dataset
 
-# full dataset (3TB of data)
-ds = load_dataset("bigcode/the-stack", split="train")
 
 # specific language (e.g. Dockerfiles)
-ds = load_dataset("bigcode/the-stack", data_dir="data/python", split="train")
-
-# dataset streaming (will only download the data as needed)
-ds = load_dataset("bigcode/the-stack", streaming=True, split="test")
+ds = load_dataset("bigcode/the-stack", data_dir="data/python", split="test")
 for sample in iter(ds): print(sample["content"])
